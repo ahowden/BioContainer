5b72f77f35e30df576f6bb405b332a9e3464c586
Andrew Howden <ahowden@email.sc.edu>
2018-06-20 10:40:31 -0400

    testing volume mounting

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ Copy ["src","main.py","./"]
 
 CMD ["python","./main.py"]
 
-CMD ["ls"]
+CMD ["ls /var/lib/docker/volumes/seq-vol/_data"]
 
 EXPOSE 80
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import sys,os,os.path,json,fnmatch,glob,shutil,time,getopt,re,csv
 from fnmatch import fnmatch
 from subprocess import *
 
-f = open('TEST.txt', 'w')
-f.close()
+path = "/var/lib/docker/volumes/seq-vol/_data/myfile"
+os.mkdir(path)
